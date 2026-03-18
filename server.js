a14ef27fd2bf7c789a3e90da9d3d2cabb014409e
Tien Anh <binkkun18@gmail.com>
2026-03-18 14:23:00 +0700

    update api danhgia

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require("express");
+    const express = require("express");
 
-const app = express();
+    const app = express();
 
-app.use(express.json());
+    app.use(express.json());
 
-const phongtroRoutes = require("./routes/phongtroroutes");
-const danhgiaRoutes = require("./routes/danhgiaroutes");
+    const phongtroRoutes = require("./routes/phongtroroutes");
+    const danhgiaRoutes = require("./routes/danhgiaroutes");
 
-app.use("/phongtro", phongtroRoutes);
-app.use("/phongtro", danhgiaRoutes);
+    app.use("/phongtro", phongtroRoutes);
+    app.use("/phongtro", danhgiaRoutes);
 
-app.listen(3000, () => {
-    console.log("🚀 Server running on port 3000");
-});
+    app.listen(3000, () => {
+        console.log("🚀 Server running on port 3000");
+    });
