c11a26fd32ae09b636d224f076ef28257a53aedd
Vu The Dat <vuthedat2005@gmail.com>
2026-03-20 01:58:14 +0700

    Fix TypeError in danhgiaController

--- controllers/danhgiaController.js
+++ controllers/danhgiaController.js
@@ -1,23 +1,23 @@
 const db = require("../config/db");
 
+// 1. Hàm lấy đánh giá (Khớp với danhgiaController.getDanhGia)
 exports.getDanhGia = (req, res) => {
-
-    const id = req.params.id;
-
-    const sql = `
-    SELECT danh_gia.*, nguoi_dung.ho_ten
-    FROM danh_gia
-    JOIN nguoi_dung
-    ON danh_gia.nguoi_dung_id = nguoi_dung.id
-    WHERE phong_id = ?
-    `;
-
-    db.query(sql, [id], (err, result) => {
-
-        if (err) return res.status(500).json(err);
-
+    const phongId = req.params.id;
+    const sql = "SELECT * FROM danh_gia WHERE phong_id = ?";
+    
+    db.query(sql, [phongId], (err, result) => {
+        if (err) return res.status(500).json({ error: err.message });
         res.json(result);
-
     });
+};
 
+// 2. Hàm thêm đánh giá (Khớp với danhgiaController.addDanhGia)
+exports.addDanhGia = (req, res) => {
+    const { phong_id, nguoi_dung_id, noi_dung, sao } = req.body;
+    const sql = "INSERT INTO danh_gia (phong_id, nguoi_dung_id, noi_dung, sao) VALUES (?, ?, ?, ?)";
+
+    db.query(sql, [phong_id, nguoi_dung_id, noi_dung, sao], (err, result) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.status(201).json({ message: "Thêm đánh giá thành công!" });
+    });
 };
